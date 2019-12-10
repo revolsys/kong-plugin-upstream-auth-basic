1fcf8b6ac80d719202df9d9b2d9647c10891a061
Wagner Daniel <Daniel.Wagner@verbund.com>
2019-12-10 09:53:50 +0100

    Simple fix for kong-plugin-upstream-auth-basic#5

--- devBuild.sh
+++ devBuild.sh
@@ -1,4 +1,4 @@
+#!/bin/sh
 sed 's/VERSION/0.0.0-0/' kong-plugin-upstream-auth-basic-VERSION.rockspec > kong-plugin-upstream-auth-basic-0.0.0-0.rockspec
 luarocks make kong-plugin-upstream-auth-basic-0.0.0-0.rockspec
 rm kong-plugin-upstream-auth-basic-0.0.0-0.rockspec
-
--- kong/plugins/upstream-auth-basic/schema.lua
+++ kong/plugins/upstream-auth-basic/schema.lua
@@ -1,7 +1,7 @@
 return {
   no_consumer = true,
   fields = {
-    username = {type = "string", required = "true"},
+    username = {type = "string", required = true},
     password = {type = "string"}
   }
 }
